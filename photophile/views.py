3cbec2db39aaddc54867b691e8b76165facaf49e
holly <holler4amarshall@hotmail.com>
2016-12-22 21:58:23 +0000

    add basic views for login and signup and add links to index

--- photophile/views.py
+++ photophile/views.py
@@ -5,6 +5,14 @@ from photophile import app
 @app.route("/")
 def get_index():
     return render_template("index.html")
+    
+@app.route("/login")
+def get_login():
+    return render_template("login.html")
+    
+@app.route("/create-account")
+def get_create_account():
+    return render_template("create_account.html")
 
     
     
