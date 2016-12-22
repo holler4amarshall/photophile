1b552a60c84ab25b32fc9aab4c9f281d5c864027
holly <holler4amarshall@hotmail.com>
2016-12-22 20:41:45 +0000

    add basic user & category tables/columns to DB

--- photophile/database.py
+++ photophile/database.py
@@ -8,3 +8,33 @@ engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
+import datetime
+
+from sqlalchemy import Column, Integer, String, Text, DateTime
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(1024))
+    email = ()
+    password = ()
+    datetime = Column(DateTime, default=datetime.datetime.now)
+    ig_oauth_token = ()
+    ig_username = ()
+    ig_fullname = ()
+    ig_profile_pic = ()
+    ig_datetime = Column(DateTime, default=datetime.datetime.now)
+
+class Category(Base):
+    __tablename__ = "categories"
+    
+    id = Column(Integer, primary_key = True)
+    category = ()
+    user_id = ()
+    media = ()
+    hero_image = ()
+
+Base.metadata.create_all(engine)
