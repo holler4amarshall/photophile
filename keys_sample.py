8da335af4c0ddb92f9a08c9f0388e79998b90655
holly <holler4amarshall@hotmail.com>
2016-12-23 00:04:14 +0000

    add a keys_sample.py file to add instagram key/secret

---
+++ keys_sample.py
@@ -0,0 +1,4 @@
+# On your local version of this application, copy this file to keys.py and enter the appropriate keys as assigned by Instagram.
+# Recommendation: Do not share your instagram keys publically
+CLIENT_ID = ""
+CLIENT_SECRET = ""
