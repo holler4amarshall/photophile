cb32611cf41b5313fe3e4efcd0cd4e5ab3c05088
holly <holler4amarshall@hotmail.com>
2016-12-23 00:24:44 +0000

    add config for instagram to project

--- photophile/config.py
+++ photophile/config.py
@@ -2,3 +2,6 @@ import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/photophile"
     DEBUG = True
+    
+class InstagramConfig(object): 
+    REDIRECT_URI = "localhost:5432/photophile/search"
